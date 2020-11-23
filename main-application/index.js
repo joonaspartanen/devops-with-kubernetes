3b2e05060faf838415479f433b925bfad5fd0056
Joonas Partanen <partanen.joonas@gmail.com>
2020-11-23 14:46:26 +0200

    Add endpoint and missing timestamp

--- main-application/index.js
+++ main-application/index.js
@@ -1,9 +1,18 @@
-var uuid = require("uuid")
+const express = require('express')
+var uuid = require('uuid')
+const app = express()
+const PORT = process.env.PORT || 8000
 
-const generateRandomString = () => {
-  const randomString = uuid.v4()
-  console.log(randomString)
+app.get('/', (req, res) => {
+  res.send('Hello!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`)
+  console.log(generateRandomStringWithTimestamp())
+  setInterval(() => console.log(generateRandomStringWithTimestamp()), 5000)
+})
+
+const generateRandomStringWithTimestamp = () => {
+  return `${new Date().toLocaleTimeString()}: ${uuid.v4()}`
 }
-
-generateRandomString()
-setInterval(generateRandomString, 5000)
